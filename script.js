852de893628e97d6beddef6c913ae9f8f09cfa1d
Zoms <martinezoms15@gmail.com>
2022-01-20 13:48:32 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,92 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+
+const count = 20;
+const apiKey = "PUucQh6XlYPuiygiZiTlUDss3tKVqVC9Ks3GIP8t9OI";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    hideLoader();
+    ready = true;
+  }
+}
+
+// helper function for setting attrbutes
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+  photosArray.forEach((photo) => {
+    const item = document.createElement("a");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank"
+    });
+
+    const img = document.createElement("img");
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.description,
+      title: photo.description
+    });
+
+    img.addEventListener("load", imageLoaded);
+
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
+
+// Loader functions
+const showloaderOnScrollEnd = () => {
+  loader.hidden = false;
+  loader.classList.remove("page-loading");
+};
+
+const hideLoader = () => {
+  loader.hidden = true;
+};
+
+//Get photos from unsplash API
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    photosArray = await response.json();
+    displayPhotos();
+  } catch (error) {
+    console.log(error);
+  } finally {
+    hideLoader();
+  }
+}
+
+// EventListeners
+
+window.addEventListener("scroll", () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+    return async () => {
+      try {
+        await getPhotos();
+        showloaderOnScrollEnd();
+      } catch (error) {
+        console.error(error);
+      } finally {
+        hideLoader();
+      }
+    };
+  }
+});
+
+getPhotos();
